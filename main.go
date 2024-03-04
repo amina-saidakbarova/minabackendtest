f6792926c42a20245b60788f3678dcefd03b605a
Your Name <you@example.com>
2024-03-04 17:42:54 +0500

    archizca naw

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Backend")
+	fmt.Println("Hello Backend2")
 
 
 }
 // ? commit ikwe sostoyaniya anjivdora
 // ? commit commana qate use kinen
-
-// 
+// ? git add .
+// ? git commit -m"message"
+// ? 
