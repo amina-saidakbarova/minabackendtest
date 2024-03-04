e561538bcf458c8c8e1ceab31d46cf531a8d934c
amina-saidakbarova <162126933+amina-saidakbarova@users.noreply.github.com>
2024-03-04 18:35:31 +0500

    NEw name main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello faxriya Backend")
-	fmt.Println("Hello Backend2")
+	fmt.Println("Hello Backend2 faxriya")
 	fmt.Println("Hello Backend3")
 	fmt.Println("----------------------")
 	fmt.Println("++++++++++++++++++++++")
